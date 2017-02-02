672ece4b98e232e17f9c48fef6f9e9de5b90124e
millertronv <senjinkoha@gmail.com>
2017-02-02 15:04:26 +0000

    Create basic user sign up - no validations, no email confirmation (user doesn't have email yet!)

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
 
 	resources :checklists
 	
+	get '/signup', to: 'users#new'
+	resources :users
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
