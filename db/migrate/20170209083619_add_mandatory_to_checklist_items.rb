a0587ae337af25a159508415161df249ba81a863
Millertron Smythe <senjinkoha@gmail.com>
2017-02-13 21:11:13 +0000

    Correct migration

--- db/migrate/20170209083619_add_mandatory_to_checklist_items.rb
+++ db/migrate/20170209083619_add_mandatory_to_checklist_items.rb
@@ -1,12 +1,5 @@
 class AddMandatoryToChecklistItems < ActiveRecord::Migration[5.0]
   def change
-    add_reference :checklist_items, :implementor, references: :users, index:true
-	add_foreign_key :checklist_items, :users, column: :implementor_id
-	
-	Implementation.all.each do |i|
-		if i.checklist
-			i.update_attribute(:implementor, i.checklist.owner)
-		end
-	end
+    add_column :checklist_items, :mandatory, :boolean
   end
 end
--- db/migrate/20170213205538_add_implementor_to_implementations.rb
+++ db/migrate/20170213205538_add_implementor_to_implementations.rb
@@ -1,4 +1,12 @@
 class AddImplementorToImplementations < ActiveRecord::Migration[5.0]
   def change
+	add_reference :implementations, :implementor, references: :users, index:true
+	add_foreign_key :implementations, :users, column: :implementor_id
+	
+	Implementation.all.each do |i|
+		if i.checklist
+			i.update_attribute(:implementor, i.checklist.owner)
+		end
+	end
   end
 end
