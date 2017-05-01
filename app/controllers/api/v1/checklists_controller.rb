54bb95b5c52f8d4ca26ee1ad028cbb9e9ae2add3
Millertron Smythe <senjinkoha@gmail.com>
2017-05-01 21:04:08 +0000

    Add tests to API checklist controller

--- app/controllers/api/v1/checklists_controller.rb
+++ app/controllers/api/v1/checklists_controller.rb
@@ -3,7 +3,6 @@ class API::V1::ChecklistsController < API::APIController
 	before_action :authenticate, except: :index
 	
 	def index
-		puts "API GET request for checklists!"
 		checklists = Checklist.all
 		render json: checklists, status: 200
 	end
--- spec/controllers/api/v1/checklists_controller_spec.rb
+++ spec/controllers/api/v1/checklists_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe API::V1::ChecklistsController, type: :controller do
-	let(:user) { FactoryGirl.create(:user) }
+	let(:user) { FactoryGirl.create(:user, password: "password", password_confirmation: "password") }
 	
 	describe "GET 'index'" do
 		it "returns a successful 200 response" do
@@ -9,8 +9,15 @@ describe API::V1::ChecklistsController, type: :controller do
 			expect(response).to be_success
 		end
 		
+		it "returns a response type of application/json" do
+			get :index, format: :json
+			expect(response.header['Content-Type']).to match /json/
+		end
+		
 		it "returns all the checklists" do
-			FactoryGirl.create(:checklist, owner: :user, name: "Checklist 1", frequency: :daily)
+			FactoryGirl.create(:checklist, owner: user, name: "Checklist 1", frequency: :daily)
+			get :index, format: :json
+			expect(response.body).to eq Checklist.all.to_json
 		end
 	end
 end
