2aabaecdd647aba797cdf03398c963c41dbd5ec8
millertronv <senjinkoha@gmail.com>
2017-05-03 12:38:14 +0100

    Update checklist API with tests

--- app/controllers/api/v1/checklists_controller.rb
+++ app/controllers/api/v1/checklists_controller.rb
@@ -3,7 +3,11 @@ class API::V1::ChecklistsController < API::APIController
 	before_action :authenticate
 	
 	def index
-		checklists = Checklist.all
-		render json: checklists, status: 200
+		if (params[:owner_id])
+			checklists = Checklist.where(owner_id: params[:owner_id])
+			render json: checklists, status: :ok
+		else
+			render json: 'Missing parameter: owner_id', status: :unprocessable_entity
+		end
 	end
 end
--- spec/controllers/api/v1/checklists_controller_spec.rb
+++ spec/controllers/api/v1/checklists_controller_spec.rb
@@ -1,23 +1,40 @@
 require 'spec_helper'
 
 describe API::V1::ChecklistsController, type: :controller do
-	let(:user) { FactoryGirl.create(:user, password: "password", password_confirmation: "password") }
+	let(:user) { FactoryGirl.create(:user) }
+	let(:other_user) { FactoryGirl.create(:user) }
+	let(:user_params) { {owner_id: user.id} }
+	context "token authenticated with API key" do
+
+		before { request.env["HTTP_AUTHORIZATION"] = token_header(user.api_key) }
+		
+		describe "GET 'index'" do
+			
+			it "requires a user ID parameter" do
+				get :index, format: :json
+				expect(response).to be_unprocessable
+			end
+			
+			context "with valid user params" do		
+				it "returns a successful 200 response" do
+					get :index, params: user_params, format: :json
+					expect(response).to be_success
+				end
+				
+				it "returns a response type of application/json" do
+					get :index, params: user_params, format: :json
+					expect(response.content_type).to eq Mime[:json]
+				end
+				
+				it "returns only the user's checklists" do
+					FactoryGirl.create(:checklist, owner: user)
+					FactoryGirl.create(:checklist, owner: other_user)
+					get :index, params: user_params, format: :json
+					expect(response.body).to eq Checklist.where(owner_id: user.id).to_json
+				end
+			end
+		end
 	
-	describe "GET 'index'" do
-		it "returns a successful 200 response" do
-			get :index, format: :json
-			expect(response).to be_success
-		end
-		
-		it "returns a response type of application/json" do
-			get :index, format: :json
-			expect(response.content_type).to eq Mime[:json]
-		end
-		
-		it "returns all the checklists" do
-			FactoryGirl.create(:checklist, owner: user, name: "Checklist 1")
-			get :index, format: :json
-			expect(response.body).to eq Checklist.all.to_json
-		end
 	end
+	
 end
