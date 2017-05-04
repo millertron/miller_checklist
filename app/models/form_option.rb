956cb675406962aa62e4ff791c5c44fc1d918cad
millertronv <senjinkoha@gmail.com>
2017-05-04 12:32:08 +0100

    Add model validation to form_option

--- app/models/form_option.rb
+++ app/models/form_option.rb
@@ -1,2 +1,4 @@
 class FormOption < ApplicationRecord
+	validates :text_value, presence: true, uniqueness: {:scope => :context, :case_sensitive => false}
+	validates :context, presence: true, uniqueness: {:case_sensitive => false}
 end
---
+++ spec/models/form_option_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe FormOption, type: :model do
+
+	context "validation" do
+		it {should validate_presence_of(:text_value)}
+		it {should validate_presence_of(:context)}
+		it {should validate_uniqueness_of(:context).case_insensitive}
+		it {should validate_uniqueness_of(:text_value).scoped_to(:context)} #some reason can't test case insensitivity with scope
+	end
+
+end
