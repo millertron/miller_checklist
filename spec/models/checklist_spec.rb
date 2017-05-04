a1665fdfe76991e382809e76e8656b4eddf901ae
Millertron Smythe <senjinkoha@gmail.com>
2017-05-04 22:14:27 +0000

    Modify checklist model

--- spec/models/checklist_spec.rb
+++ spec/models/checklist_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 describe Checklist do
+	let(:checklist) { FactoryGirl.create(:checklist) }
+	
 	context "validation" do
 		it {should validate_presence_of(:name)}
 		it {should validate_presence_of(:frequency)}
 		it {should belong_to(:owner)}
 		it {should validate_uniqueness_of(:name).scoped_to(:owner_id)}
+	end
+	
+	context "#checklist_item" do
+		it "should return nil if checklist has no checklist items" do
+			expect(checklist.checklist_item).to eq nil
+		end
+		
+		it "should return the first checklist item from its collection of checklist items" do
+			checklist_item = FactoryGirl.create(:checklist_item, checklist: checklist)
+			checklist.update_attribute(:checklist_items, checklist.checklist_items << checklist_item)
+			expect(checklist.checklist_item).to eq checklist_item
+		end
+		
 	end
 	
 end
