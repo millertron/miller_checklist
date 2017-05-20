3e5159df6b8bdd727d3c829b9aa1716ce393f4b6
millertronv <senjinkoha@gmail.com>
2017-05-20 17:40:29 +0100

    Enable default mail in development envrionment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
 	def activation user
 		@user = user
-		mail to: @user.email, subject: "MUTEX Notification: User account activation"
+		mail to: ENV["DEV_MAIL_OVERRIDE"] || @user.email, subject: "MUTEX Notification: User account activation"
 	end
 end
